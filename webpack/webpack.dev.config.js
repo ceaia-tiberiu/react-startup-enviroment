3fdfe553800e53286c52ff88c8679b3393fc335a
Tiberiu Ceaia <tiberiu.ceaia@lexisnexis.com>
2017-11-22 12:34:07 +0200

    All you need for a start-up in react app

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,33 @@
+let webpack = require('webpack');
+let path = require('path');
+
+let parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+                
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
